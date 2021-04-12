3d91f366c3f71489dd6e8123006ac639e1c9f571
Peter Hudson <peter.hudson@genomicsengland.co.uk>
2021-04-12 17:22:48 +0100

    NTOS-5556: updated lint package to correct version

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 module.exports = {
-  extends: ['plugin:react/recommended', '@zonedigital/eslint-config-zone'],
-  plugins: ['react-hooks'],
+  extends: [
+    '../eslint-config-zone',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/react',
+  ],
+  plugins: ['react-hooks', 'jsx-a11y'],
   rules: {
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
   },
-}
+};
