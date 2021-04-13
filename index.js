b67b1c47ad27ee5c653659a019bbae83ae1ee5fe
Peter Hudson <peter.hudson@genomicsengland.co.uk>
2021-04-13 16:00:48 +0100

    NTOS-555: updated references for version 4.2.1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./config')
+module.exports = {
+  extends: [
+    '@zonedigital/eslint-config-zone',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: ['react-hooks', 'jsx-a11y'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+  },
+};
